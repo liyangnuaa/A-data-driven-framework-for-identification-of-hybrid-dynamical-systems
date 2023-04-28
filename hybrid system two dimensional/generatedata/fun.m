50a7fb6bcb8079f95c41e64fa88308850879d937
liyangnuaa <61436455+liyangnuaa@users.noreply.github.com>
2023-04-28 14:59:41 +0800

    Delete hybrid system two dimensional directory

--- hybrid system two dimensional/generatedata/fun.m
+++
@@ -1,8 +0,0 @@
-function y=fun(~,x)
-
-global gamma
-
-y=zeros(size(x));
-y(1)=-x(1);
-y(2)=-gamma*(1+x(1)^2)*(x(2)-3);
-
--- hybrid system two dimensional/generatedata/fun2.m
+++
@@ -1,8 +0,0 @@
-function y=fun2(~,x)
-
-global gamma
-
-y=zeros(size(x));
-y(1)=-x(1);
-y(2)=-gamma*(1+x(1)^2)*(x(2)+3);
-

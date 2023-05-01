055dec0a6f07c4f8579b9e9e0c79ba155f0a03d4
liyangnuaa <61436455+liyangnuaa@users.noreply.github.com>
2023-05-01 09:34:38 +0800

    Delete hybrid system 3-dimensional missile vary Ft directory

--- hybrid system 3-dimensional missile vary Ft/generatedata/rk4.m
+++
@@ -1,6 +0,0 @@
-function xout=rk4(t0,h,x0)
-k1=h*fun(t0,x0);
-k2=h*fun(t0+h/2,x0+0.5*k1);
-k3=h*fun(t0+h/2,x0+0.5*k2);
-k4=h*fun(t0+h,x0+k3);
-xout=x0+(k1+2*k2+2*k3+k4)/6;
--- hybrid system 3-dimensional missile vary Ft/generatedata/rk4_2.m
+++
@@ -1,6 +0,0 @@
-function xout=rk4_2(t0,h,x0)
-k1=h*fun2(t0,x0);
-k2=h*fun2(t0+h/2,x0+0.5*k1);
-k3=h*fun2(t0+h/2,x0+0.5*k2);
-k4=h*fun2(t0+h,x0+k3);
-xout=x0+(k1+2*k2+2*k3+k4)/6;
